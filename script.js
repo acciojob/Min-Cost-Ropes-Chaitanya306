a8da83012b939282b0e2207737e339b5c33b8d24
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-20 18:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let totalcost=0
+	arr.sort((a,b)=>a-b)
+	arr.reduce((total,num)=>{
+		totalcost+=(total+num)
+		return (total+num)
+	},0)
+	return totalcost  
 }
 
 module.exports=mincost;
